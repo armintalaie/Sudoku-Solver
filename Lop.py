71cacf8d7a510183eb6e4f6fd777eb4d9c973140
armintalaie <armin.talaie@alumni.ubc.ca>
2020-06-08 03:10:27 -0700

    added more methods of solving: - section_sudoku function checks a square and analyzes all cells,rows and columns to find a number that could be assigned to one cell - before trial and error, the program now tries to find only-one-option cells and the cells from the function above until no new cells can be solved: these additions can solve from a few to a lot of the cells, and therefore reduce the time taken for the trial and error as there's less cells to check

--- Lop.py
+++ Lop.py
@@ -43,8 +43,12 @@ def update_sudoku(sudoku):
         row_or_col_options(sudoku, i, True)
 
 
+def find_cell_options(i, m):
+    return list(set(all_row_options[i]) & set(all_col_options[m]) &
+                set(all_square_options[(i - i % 3) + int((m - m % 3) / 3)]))
+
+
 def iterate_options(sudoku, num):
-    changed = False
     finished = True
     for i in range(9):
         for m in range(9):
@@ -53,12 +57,10 @@ def iterate_options(sudoku, num):
             if sudoku[i][m] == 0:
                 finished = False
 
-            # if i == 1 and m == 4:
             # print(all_row_options[i])
             # print(all_col_options[m])
             # print(all_square_options[(i - i % 3) + int((m - m % 3) / 3)])
-            cell_options = list(set(all_row_options[i]) & set(all_col_options[m]) &
-                                set(all_square_options[(i - i % 3) + int((m - m % 3) / 3)]))
+            cell_options = find_cell_options(i, m)
             # print(str(i) + " tt " + str(m) + "   " + str(cell_options))
             ra = len(cell_options)
             if ra == 0:
@@ -73,7 +75,6 @@ def iterate_options(sudoku, num):
                     # print(str(cell_options) + "    " +  str(sudoku[i][m]))
 
                     if iterate_options(sudoku, num):
-                        changed = True
                         return True
 
                     else:
@@ -83,37 +84,103 @@ def iterate_options(sudoku, num):
                         all_square_options[(i - i % 3) + int((m - m % 3) / 3)].append(cell_options[p])
                         sudoku[i][m] = 0
                         # print(str(cell_options) + "    " + str(sudoku[i][m]))
+                return False
 
-                if not changed:
-                    # print("meowwwwww")
-                    return False
+    return finished
 
-    if finished:
-        return True
+
+def initial_solve(sudoku):
+    changed = False
+    for i in range(9):
+        for m in range(9):
+            if sudoku[i][m] != 0:
+                continue
+            cell_options = find_cell_options(i, m)
+            if len(cell_options) == 1:
+                all_row_options[i].remove(cell_options[0])
+                all_col_options[m].remove(cell_options[0])
+                all_square_options[(i - i % 3) + int((m - m % 3) / 3)].remove(cell_options[0])
+                sudoku[i][m] = cell_options[0]
+                print("yes momma")
+                print(i)
+                print(m)
+                changed = True
+    return changed
+
+
+def naked_pairs(sudoku):
+    pass
+
+
+def section_check(sudoku):
+    changed = False
+
+    for w in range(0, 3):
+        for q in range(0, 3):
+            answers = [[z, 0, -1, -1] for z in range(1, 10)]
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if sudoku[i + w*3][j + q*3] != 0:
+                        continue
+
+                    cell_options = find_cell_options(i + w*3, j + q*3)
+
+                    for k in cell_options:
+                        answers[k - 1][1] += 1
+                        answers[k - 1][2] = i + w*3
+                        answers[k - 1][3] = j + q*3
+
+            for i in range(9):
+                if answers[i][1] == 1:
+                    sudoku[answers[i][2]][answers[i][3]] = answers[i][0]
+                    # print("next" + str(i) + "oo" + str(j) + "  " + str(w) + "  " + str(q))
+                    all_square_options[w*3 + q].remove(answers[i][0])
+                    all_row_options[answers[i][2]].remove(answers[i][0])
+                    all_col_options[answers[i][3]].remove(answers[i][0])
+                    changed = True
+                    print(answers[i][2])
+                    print(answers[i][3])
+                    # print(answers[i])
+                    print("yepp")
+                    print_sudoku(sudoku)
+    return changed
 
 
 def solve_sudoku(sudoku):
     update_sudoku(sudoku)
+    changing = True
+
+    while changing:
+        if initial_solve(sudoku) or section_check(sudoku):
+            print("yes")
+            continue
+        else:
+            changing = False
+
+    naked_pairs(sudoku)
     iterate_options(sudoku, 9)
 
 
+def print_sudoku(sudoku):
+    for k in range(9):
+        print(sudoku[k])
+
+
 # running program
-with open("SampleSudoku's", "r") as File1:
+with open("s2", "r") as File1:
     sudoku_input = [[0 for _ in range(9)] for _ in range(9)]
     temp = File1.readlines()
-    for j, line in enumerate(temp):
+    for n, line in enumerate(temp):
         for i in range(0, 9):
-            sudoku_input[j][i] = int(line[i:i + 1])
+            sudoku_input[n][i] = int(line[i:i + 1])
 
 print("input:")
-for k in range(9):
-    print(sudoku_input[k])
+print_sudoku(sudoku_input)
 
 solve_sudoku(sudoku_input)
 
 print("solution:")
-for k in range(9):
-    print(sudoku_input[k])
+print_sudoku(sudoku_input)
 
 # TODO: add more sample sudoku's to solve
 # TODO: add the ability to also check with upper/lower rows/column for a narrower margin of options
